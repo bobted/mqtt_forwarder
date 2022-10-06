a7a310b43e79f5efc1b4fb947394d1bfe0087b4b
Todd Pike <todd.pike@llamasoft.com>
2022-10-05 20:16:49 -0400

    updates to script and adding dev container config

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
-FROM seblucas/alpine-python3:latest
-LABEL maintainer="Sebastien Lucas <sebastien@slucas.fr>"
-LABEL Description="mqtt_forwarder image"
+FROM python:3.9.14
 
-COPY *.py /usr/bin/
+WORKDIR /app
+COPY *.py /app
 
-RUN chmod +x /usr/bin/mqtt_forwarder.py
+RUN pip install -r requirements.txt
 
-ENTRYPOINT ["python3", "-u", "/usr/bin/mqtt_forwarder.py"]
-CMD ["--help"]
+RUN chmod +x /app/mqtt_forwarder.py
+
+ENV MQTT_HOST="172.16.2.10" \
+    MQTT_USER="mqtt" \
+    MQTT_PWD="A30b979f2B#8" \
+    MQTT_DEST_BASE="hubitat/34-e1-d1-80-0c-f2-000d/mqtt-garage-door-819/cmd" \
+    MQTT_SOURCE_TOPIC="homebridge/myq/CG083017EDB7/#" \
+    MQTT_FORWARDER_HASHMAP='{ "garagedoor": "contactSensor" }'
+
+ENTRYPOINT ["python3", "-u", "/app/mqtt_forwarder.py"]
--- mqtt_forwarder.py
+++ mqtt_forwarder.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
-#
-#  mqtt_forwarder.py
-#
-#  Copyright 2016 Sébastien Lucas <sebastien@slucas.fr>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-
 
 import os, re, time, json, argparse, signal
 import paho.mqtt.client as mqtt # pip install paho-mqtt
 import urllib.parse
 
 verbose = False
+
+CONNECTION_RETURN_CODE = [
+    "connection successful",
+    "incorrect protocol version",
+    "invalid client identifier",
+    "server unavailable",
+    "bad username or password",
+    "not authorised",
+]
+
+def parseArgs():
+  parser = argparse.ArgumentParser(description='Send MQTT payload received from a topic to firebase.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+  parser.add_argument('-m', '--mqtt-host',   dest='host',        action="store",       default="127.0.0.1",    help='Specify the MQTT host to connect to.', **environ_or_required('MQTT_HOST'))
+  parser.add_argument('-u', '--username',    dest='username',    action="store",       metavar="USERNAME",     help='MQTT boroker login username',          **environ_or_required('MQTT_USER'))
+  parser.add_argument('-p', '--password',    dest='password',    action="store",       metavar="$ECRET",       help='MQTT boroker login password',          **environ_or_required('MQTT_PWD'))
+  parser.add_argument('-P', '--port',        dest='port',        action="store",       type=int, default=1883, metavar=1883, help='MQTT boroker port')
+  parser.add_argument('-a', '--hash-map',    dest='hashMap',     action="store",       help='Specify the map of MQTT topics to forward.',                   **environ_or_required('MQTT_FORWARDER_HASHMAP'))
+  parser.add_argument('-n', '--dry-run',     dest='dryRun',      action="store_true",  default=False,          help='No data will be sent to the MQTT broker.')
+  parser.add_argument('-d', '--destination', dest='destination', action="store",       default="sensor raw",   help='The destination MQTT topic base.',     **environ_or_required('MQTT_DEST_BASE'))
+  parser.add_argument('-t', '--topic',       dest='topic',       action="store",       default="sensor/esp/#", help='The listening MQTT topic.',            **environ_or_required('MQTT_SOURCE_TOPIC'))
+  parser.add_argument('-v', '--verbose',     dest='verbose',     action="store_false", default=True,          help='Enable debug messages.')
+
+  return parser.parse_args()
 
 def signal_handler(signal, frame):
   print('You pressed Ctrl+C!')
@@ -39,64 +42,50 @@ def environ_or_required(key):
 
 def debug(msg):
   if verbose:
-    print (msg + "\n")
+    print (msg)
 
 def on_connect(client, userdata, flags, rc):
-    debug("Connected with result code "+str(rc))
+  debug("Connected with result: " + CONNECTION_RETURN_CODE[rc] if rc < len(CONNECTION_RETURN_CODE) else rc)
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe(args.topic)
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+  client.subscribe(args.topic)
 
 def on_message(client, userdata, msg):
-    sensorName = msg.topic.split('/') [-1]
-    if sensorName in hashMap.keys():
-        tstamp = int(time.time())
-        mqttPath = urllib.parse.urljoin(args.destination + '/', hashMap[sensorName])
-        debug("Received message from {0} with payload {1} to be published to {2}".format(msg.topic, str(msg.payload), mqttPath))
-        nodeData = msg.payload
-        newObject = json.loads(nodeData.decode('utf-8'))
-        newObject['time'] = tstamp
-        nodeData = json.dumps(newObject)
-        if not args.dryRun:
-          client.publish(mqttPath, nodeData)
-        else:
-          debug("Dry run")
+  sensorName = msg.topic.split('/') [-1]
+  if sensorName in hashMap.keys():
+    tstamp = int(time.time())
+    mqttPath = urllib.parse.urljoin(args.destination + '/', hashMap[sensorName])
+    debug("Received message from {0} with payload {1} to be published to {2}".format(msg.topic, str(msg.payload), mqttPath))
+    nodeData = msg.payload
+    ##newObject = json.loads(nodeData.decode('utf-8'))
+    ##newObject['time'] = tstamp
+    ##nodeData = json.dumps(newObject)
+    if not args.dryRun:
+      debug("Sending message...")
+      client.publish(mqttPath, nodeData)
     else:
-        debug("Received message from {0} with payload {1}. Hash not found in hashMap".format(msg.topic, str(msg.payload)))
+      debug("Dry run")
+  else:
+    debug("Received message from {0} with payload {1}. Hash not found in hashMap".format(msg.topic, str(msg.payload)))
 
+if __name__ == '__main__':
+  args = parseArgs()
 
+  signal.signal(signal.SIGINT, signal_handler)
+  signal.signal(signal.SIGTERM, signal_handler)
 
-parser = argparse.ArgumentParser(description='Send MQTT payload received from a topic to firebase.', 
-  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-m', '--mqtt-host', dest='host', action="store", default="127.0.0.1",
-                   help='Specify the MQTT host to connect to.')
-parser.add_argument('-a', '--hash-map', dest='hashMap', action="store",
-                   help='Specify the MQTT host to connect to.',
-                   **environ_or_required('MQTT_FORWARDER_HASHMAP'))
-parser.add_argument('-n', '--dry-run', dest='dryRun', action="store_true", default=False,
-                   help='No data will be sent to the MQTT broker.')
-parser.add_argument('-d', '--destination', dest='destination', action="store", default="sensor/raw",
-                   help='The destination MQTT topic base.')
-parser.add_argument('-t', '--topic', dest='topic', action="store", default="sensor/esp/#",
-                   help='The listening MQTT topic.')
-parser.add_argument('-T', '--topic-error', dest='topicError', action="store", default="error/transformer", metavar="TOPIC",
-                   help='The MQTT topic on which to publish the message (if it wasn\'t a success).')
-parser.add_argument('-v', '--verbose', dest='verbose', action="store_true", default=False,
-                   help='Enable debug messages.')
+  verbose = args.verbose
+  hashMap = json.loads(args.hashMap)
 
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
 
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
-args = parser.parse_args()
-verbose = args.verbose
-hashMap = json.loads(args.hashMap)
+  if args.username is not None:
+      client.username_pw_set(args.username, password=args.password)
+  elif args.password is not None:
+      raise Exception('Login with password requires username.')
+  client.connect(host=args.host, port=args.port, keepalive=60)
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect(args.host, 1883, 60)
-
-client.loop_forever()
-
+  client.loop_forever()
